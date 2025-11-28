2949e1b788c38085ab9502e46e412c2c7780c06d
Azeezfasasi <fas.azeez@gmail.com>
2025-11-28 08:23:08 +0100

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,10 @@ import { AuthProvider } from "@/context/AuthContext";
 export const metadata = {
   title: 'Rayob Engineering & Mgt. Co. Ltd',
   description: 'Innovative Engineering Solutions for Modern Projects.',
+  icons: {
+    icon: '/rayob1.jpg',
+    apple: '/apple-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
